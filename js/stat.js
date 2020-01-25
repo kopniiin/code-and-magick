c084cba26d6e82fcc384dd9f822d19780a84ca9b
kopniiin <kopniiin@gmail.com>
2020-01-25 22:10:04 +0300

    Исправляет замечания наставника

--- js/stat.js
+++ js/stat.js
@@ -1,100 +1,101 @@
 'use strict';
 
-var WINDOW_X = 100;
-var WINDOW_Y = 10;
-var WINDOW_WIDTH = 420;
-var WINDOW_HEIGHT = 270;
-var GAP = 16;
-var WINDOW_COLOR = '#ffffff';
-
-var SHADOW_OFFSET = 10;
-var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-
 var FONT = '16px PT Mono';
 var FONT_GAP = 16;
 var FONT_COLOR = '#000000';
 
-var MESSAGE_TEXTS = [
-  'Ура вы победили!',
-  'Список результатов:'
-];
+var WINDOW_X = 100;
+var WINDOW_Y = 10;
+var WINDOW_WIDTH = 420;
+var WINDOW_HEIGHT = 270;
+var WINDOW_GAP = 16;
+var WINDOW_TITLE = 'Ура вы победили!';
+var WINDOW_BACKGROUND_COLOR = '#ffffff';
+var windowTitleX = WINDOW_X + WINDOW_GAP;
+var windowTitleY = WINDOW_Y + WINDOW_GAP + FONT_GAP;
+
+var SHADOW_OFFSET = 10;
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var shadowX = WINDOW_X + SHADOW_OFFSET;
+var shadowY = WINDOW_Y + SHADOW_OFFSET;
 
 var BAR_WIDTH = 40;
-var MAX_BAR_HEIGHT = 150;
+var BAR_MAX_HEIGHT = 150;
 var BAR_GAP = 50;
+var BAR_CHART_TITLE = 'Список результатов:';
 var BAR_HUE = 240;
 var BAR_LIGHTNESS = 50;
+var barChartTitleX = WINDOW_X + WINDOW_GAP;
+var barChartTitleY = WINDOW_Y + WINDOW_GAP + FONT_GAP * 2;
+var barNameY = WINDOW_Y + WINDOW_HEIGHT - WINDOW_GAP;
 
 var CURRENT_PLAYER_NAME = 'Вы';
 var CURRENT_PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
 
-var renderWindow = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, WINDOW_WIDTH, WINDOW_HEIGHT);
-};
+var renderWindow = function (ctx) {
+  renderShadow(ctx);
 
-var renderMessage = function (ctx) {
+  ctx.fillStyle = WINDOW_BACKGROUND_COLOR;
+  ctx.fillRect(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+
   ctx.font = FONT;
   ctx.fillStyle = FONT_COLOR;
-
-  var messageX = WINDOW_X + GAP;
-
-  for (var i = 0; i < MESSAGE_TEXTS.length; i++) {
-    var text = MESSAGE_TEXTS[i];
-    var messageY = WINDOW_Y + GAP + FONT_GAP * (i + 1);
-    ctx.fillText(text, messageX, messageY);
-  }
+  ctx.fillText(WINDOW_TITLE, windowTitleX, windowTitleY);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var renderShadow = function (ctx) {
+  ctx.fillStyle = SHADOW_COLOR;
+  ctx.fillRect(shadowX, shadowY, WINDOW_WIDTH, WINDOW_HEIGHT);
+};
+
+var getMaxNumber = function (arr) {
+  var maxNumber = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
-    maxElement = (maxElement < arr[i]) ? arr[i] : maxElement;
+    maxNumber = (maxNumber < arr[i]) ? arr[i] : maxNumber;
   }
 
-  return maxElement;
+  return maxNumber;
 };
 
-var getRandomColor = function () {
+var getRandomBarColor = function () {
   var saturation = Math.floor(Math.random() * 101);
   return 'hsl(' + BAR_HUE + ', ' + saturation + '%, ' + BAR_LIGHTNESS + '%)';
 };
 
-var renderBar = function (ctx, barX, nameY, name, time, barHeight, barColor) {
-  var barY = nameY - (FONT_GAP + barHeight);
-  var timeY = barY - FONT_GAP;
+var renderBar = function (ctx, barName, barValue, barX, barHeight, barColor) {
+  var barY = barNameY - (FONT_GAP + barHeight);
+  var valueY = barY - FONT_GAP;
 
   ctx.fillStyle = FONT_COLOR;
-  ctx.fillText(name, barX, nameY);
-  ctx.fillText(time, barX, timeY);
+  ctx.fillText(barName, barX, barNameY);
+  ctx.fillText(barValue, barX, valueY);
 
   ctx.fillStyle = barColor;
   ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
 };
 
-var renderBarChart = function (ctx, names, times) {
-  var maxTime = getMaxElement(times);
+var renderBarChart = function (ctx, barNames, barValues) {
+  ctx.fillStyle = FONT_COLOR;
+  ctx.fillText(BAR_CHART_TITLE, barChartTitleX, barChartTitleY);
 
-  for (var i = 0; i < names.length; i++) {
-    var barX = WINDOW_X + GAP + (BAR_WIDTH + BAR_GAP) * i;
-    var nameY = WINDOW_Y + WINDOW_HEIGHT - GAP;
-    var name = names[i];
-    var time = Math.round(times[i]);
-    var barHeight = (times[i] / maxTime) * MAX_BAR_HEIGHT;
-    var barColor = (name === CURRENT_PLAYER_NAME) ?
+  var maxBarValue = getMaxNumber(barValues);
+
+  for (var i = 0; i < barNames.length; i++) {
+    var barName = barNames[i];
+    var barValue = Math.round(barValues[i]);
+    var barX = WINDOW_X + WINDOW_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var barHeight = (barValues[i] / maxBarValue) * BAR_MAX_HEIGHT;
+    var barColor = (barName === CURRENT_PLAYER_NAME) ?
       CURRENT_PLAYER_COLOR :
-      getRandomColor();
+      getRandomBarColor();
 
-    renderBar(ctx, barX, nameY, name, time, barHeight, barColor);
+    renderBar(ctx, barName, barValue, barX, barHeight, barColor);
   }
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderWindow(ctx, WINDOW_X + SHADOW_OFFSET, WINDOW_Y + SHADOW_OFFSET, SHADOW_COLOR);
-  renderWindow(ctx, WINDOW_X, WINDOW_Y, WINDOW_COLOR);
-
-  renderMessage(ctx);
+  renderWindow(ctx);
 
   renderBarChart(ctx, names, times);
 };
